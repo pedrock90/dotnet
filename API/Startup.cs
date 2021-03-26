39cdcefc13b89d89dbb6769b647276c7b1add701
pedrock90 <pedro.cardenas.benavides@gmail.com>
2021-03-26 14:35:52 -0600

    Episode 25 users on angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,11 +33,13 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-            services.AddControllers();
+            services.AddControllers();    
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
